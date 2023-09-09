b1b8738a573a2b5f2ec1a2837060d317786ce62a
anveshdikondwar <anveshdikondwar@gmail.com>
2023-09-09 20:00:28 +0530

    Anvesh

---
+++ src/main/java/org/geekster/MailConstants.java
@@ -0,0 +1,7 @@
+package org.geekster;
+
+public class MailConstants {
+
+    public static final String SENDER = "anveshdikondwar@gmail.com";
+    public static final String APP_PASSWORD = "hynbantysnyn" ;
+}
